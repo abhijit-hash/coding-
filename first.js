4038f422b12b23e6db5e42ab3a9494dcc3430909
Abhijit Pal <subujpaul2003@gmail.com>
2023-02-11 15:19:39 +0530

    commit no-1

---
+++ first.js
@@ -0,0 +1,4 @@
+const arr=[2,4,6,8,10];
+for(let element of arr){
+    console.log(element);
+}
---
+++ say_something.py
@@ -0,0 +1,10 @@
+# importing the pyttsx library
+import pyttsx3
+
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+engine.say("hello i am ahbijit")
+engine.say("Thank you, Geeksforgeeks")
+engine.runAndWait()

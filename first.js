b9767a93d2e4fd0e17077339dc0a85ab906b4215
Abhijit Pal <subujpaul2003@gmail.com>
2023-02-11 15:53:43 +0530

    somechanges in code

--- first.js
+++ first.js
@@ -2,3 +2,4 @@ const arr=[2,4,6,8,10];
 for(let element of arr){
     console.log(element);
 }
+console.log("hello world!");
--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main ()
 {
     cout<<"Hello World"<<endl;
+    cout<<"basics c++"<<endl;
 
     return 0;
 }
